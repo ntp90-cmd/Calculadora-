0b2d3243b5966f2e0d5fde2ebef35b4d15556289
ntp90-cmd <n.totpaiz30@gmail.com>
2026-09-04 07:49:22 -0600

    Add files via upload
    Aqui se encuentra el codigo y todo lo que esta relacionado con el ejercicio de la calculadora.

---
+++ calculadora/src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author Natanael
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+    calculadora1 i = new calculadora1();
+    i.setLocationRelativeTo(null);
+    i.setVisible(true);
+}
+}
